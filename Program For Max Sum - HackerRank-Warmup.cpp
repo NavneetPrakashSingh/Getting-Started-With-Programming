b24f801139bd745cd4ca0af8fa0ce94d18b36828
navneet94 <npsc009@gmail.com>
2018-03-07 11:43:31 +0530

    Max-Min Sum
    Max-Min Sum HackerRank Warmup

--- Program For Max Sum - HackerRank-Warmup.cpp
+++ Program For Max Sum - HackerRank-Warmup.cpp
@@ -1,38 +1,32 @@
 //min max sum hackerrank warmup https://www.hackerrank.com/challenges/mini-max-sum/problem
 #include<bits/stdc++.h>
+#include<stdint.h>
 
 using namespace std;
-void minMaxSum(vector<int>A){
+void minMaxSum(vector<unsigned long int>A){
 //	cout<<A.size();
-	int ans=0,max_value=0,min_value=0;
-	vector<int> array_sum(5);
-	int count = 0;
-	int position =0;
-	for(int outer_index = 0; outer_index<A.size();outer_index++){
-		int sum =0;
-		for(int index=0;index<A.size();index++){
-			if(index == outer_index){
-				
-			}else{
-				sum = sum + A[index];	
-			}
-		}
-		if(max_value<sum){
-			max_value = sum;
-		}
-		if(count==0){			
-			min_value = sum;
-			count++;
-		}
-		if(min_value>sum){
-			min_value = sum;
-		}
+	vector<unsigned long int> temp_vector (A.size());
+	unsigned long int sum=0;
+	for(int index=0;index<A.size();index++){
+		sum = sum+A[index];
 	}
-	cout<<min_value<<" "<<max_value;
+	unsigned long int current_sum=0;
+	for(int i=0;i<A.size();i++){
+		current_sum = sum-A[i];
+		temp_vector[i] = current_sum;
+	}
+	
+
+	sort(temp_vector.begin(), temp_vector.end());
+	for(int i=0;i<temp_vector.size();i++){
+		cout<<temp_vector[i]<<endl;
+	}
+	cout<<temp_vector[0]<<" "<<temp_vector[temp_vector.size()-1];
+//	cout<<min_value<<" "<<max_value;
 }
 
 int main(){
-	vector<int> arr(5);
+	vector<unsigned long int> arr(5);
 	for(int i=0;i<5;i++){
 		cin>>arr[i];
 	}
